ff04ebef186b952475c9f50eba40ef7465b13d7b
Dmitrijv <dmitrij.vel@live.se>
2021-01-20 16:29:44 +0100

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>fend19-js4-gitjobs</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
